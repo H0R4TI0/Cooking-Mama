c32fd9c8921b2447eb6865d59133ca473c2409af
H0R4TI0 <38559023+H0R4TI0@users.noreply.github.com>
2018-04-24 12:47:26 +0800

    Update copekterussiap.c

--- copekterussiap.c
+++ copekterussiap.c
@@ -5,7 +5,7 @@ void Table(int,int,int[]);
 void PrepTable(int, int[]);
 void ChooseTable (int[], int, int, int);
 void Menu (char [9][50], float [9]);
-void Order (char[9][50], int[][9], int[9]);
+void Order (char[9][50], int[][9], int[9], float []);
 void Stats (char[9][50], float [], int[]);
 void Checkout(int [][9], char [9][50],int [],float[]);
 void SetupPrice(char fname[9][50],float price[9]);
@@ -14,8 +14,8 @@ int i,max;
 int  main ()
 {
 	system("color 0");
-	char input,cus,opt,person,choice;
-	int check,id,total=0,a=4,b=3,x,y,reset=0;
+	char input,cus,opt,person,choice,staff[3][30]={"REZZZZZAAAAMMMIIIRRRRR","IZZZZZAAAAAAHHHHHHH","GRAND MASTER BIG BOSS"};
+	int check,id,total=0,a=4,b=3,x,y,reset=0,staf;
 	char fname[9][50]={"Skyjuice","Chicken Chop","Nasi Lemak","Nugget Crispy","Maggi Goreng","Nasi Ayam ","French Toast","Spaghetti","Nasi Bujang"};
   	float price[9]={1.00,11.5,12.5,13.5,14.5,15.5,16.5,17.5,18.5}; max=a*b;
 	system("title #####   CAFE RATATOUILLE   #####");
@@ -24,10 +24,12 @@ int  main ()
 	
 	if(reset==1)
 	{
-		
+		system("cls");
+		system("color C");
 		printf("\n\nInsert Restaurant table dimensions (Length X Width)\n-");
 		scanf("%d %d",&a,&b);
-		system("cls");
+		system("cls"); 
+		system("color 7");
 		printf("\n\nTable Setting Successul\n");
 		max=a*b;
 		int table[max];
@@ -56,37 +58,38 @@ int  main ()
 	
 do
 {	
-
-	printf("\t\tWELCOME TO CAFE RATATOUILLE\n");
-	printf("\nAre you a customer or a staff(c/s)?\n");
+	system("color E");
+	printf("\n\t\t===========================\n\n\t\tWELCOME TO CAFE RATATOUILLE\n\n\t\t===========================");
+	printf("\n\n\nAre you a [c]ustomer or a [s]taff?\n");
 	input=getch(); fflush(stdin);
 	
 	if((input=='c')||(input=='C'))
 	{
 		max=a*b;
-		printf("Are you an existing or new customer(e/n)?");
+		printf("\nAre you an [e]xisting or [n]ew customer?");
 		cus=getch();fflush(stdin);
 		if(cus=='n'||cus=='N')
 		{
 			system("cls");
-			printf("~~~Available tables~~~~~~");
+			printf("\n\t\t~~~Available tables~~~~~~\n");
 			Table(a,b,table);
 			ChooseTable(table,max,a,b);
 	        Menu(fname,price);
+	        system("color B");
 	        printf("\nOrder Now!\n");
-	        Order(fname,quan,totquan);
+	        Order(fname,quan,totquan,price);
 		}
 		else if(cus=='e'||cus=='E')
 		{   printf("\n\nEnter your table number: ");
 			scanf("%d",&i); i-=1;
 			
-			printf("~~~~~~~%d~~~~~~",table[i]);
 			if(table[i]<0)
 		{
 			
-			printf("\nOrder or Check Out (o/c)?\n-");
+			printf("\n\n[O]rder or [C]heck Out?\n");
 			choice=getch();fflush(stdin);
 			system("cls");
+			system("color A");
 			
 			
 			switch (choice)
@@ -94,14 +97,15 @@ do
 				case 'o' :printf("\n\n");
 					  	  printf("\nOrder Now!\n"); 
 				          Menu(fname,price);
-						  Order(fname,quan,totquan); break;
+						  Order(fname,quan,totquan,price); break;
 						  
 						  
 				case 'c' :printf("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\tRECIEPT\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 				Checkout(quan,fname,table,price);break;
 				
-				case 'O' :printf("\n\n"); Menu(fname,price);
-						  Order(fname,quan,totquan); break;
+				case 'O' :printf("\n\n");
+				 		  Menu(fname,price);
+						  Order(fname,quan,totquan,price); break;
 						  
 						  
 				case 'C' :printf("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\tRECIEPT\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
@@ -111,29 +115,36 @@ do
 				default : printf("Invalid choice!");
 				
 			}
+			system("pause");
 		}
-		else printf("\n\nInvalid Table!\n");
+		else 
+		{printf("\n\nInvalid Table!\n\n\n");
+		system("pause");
 		}
-	system("pause");system("cls");	
+		}
+		system("cls");
 	}
 	else if(input=='s'||input=='S')
 	{	
 	    do
 	    {check=0;
-		printf("Enter staff ID :");
+		printf("\n\nEnter staff ID :");
 		scanf("%d",&id);
 		switch(id)
-		{case(172951):break;
-		 case(171679):break;
-		 case(0):break;
+		{case(172951):staf=0;break;
+		 case(171679):staf=1;break;
+		 case(0):staf=2;break;
 		 default:printf("Invalid ID!\n");check=1;
 		}
 		}while(check==1);
 		
-	    printf("WELCOME STAFF!\n");
-		printf("[S]etup Restraunt Size\n");
-		printf("[R]evenue\n");
-		printf("[P]rice Setup\n");
+		system("cls");
+		system("color D");
+	    printf("\n\n\t\tWELCOME %s!\n\n\nOptions Available:\n",staff[staf]);
+		printf("\n\n\n\t\t[S]etup Restraunt Size\n");
+		printf("\n\n\n\t\t[R]evenue\n");
+		printf("\n\n\n\t\t[P]rice Setup\n");
+		printf("\n\n\nPress [X] to exit");
 		choice=getch();
 		choice=toupper(choice);
 		if(choice=='S')
@@ -142,20 +153,32 @@ do
 	}
 		
 		else if(choice=='R')
-		{Stats(fname,price,totquan);
+		{
+		system("cls");
+		system("color F");
+		Stats(fname,price,totquan);
+		printf("\n\n\n");
 		system("pause");
-		system("cls");}
+		system("cls");
+		}
 		
 		else if(choice=='P')
-		{SetupPrice(fname,price);
-		 system("pause");
-		 system("cls");}
-				
+		{
+		system("color 9");
+		SetupPrice(fname,price);
+	    system("pause");
+	    system("cls");
+		}	
+		 
+		 else 
+		 system("cls");
+		
 	}
 	
-	else system("cls");
+	else 
+	system("cls");
 	
-		;
+		
 } while(reset!=1);
 }while(1);
 }
@@ -169,9 +192,10 @@ void Stats(char fname[9][50], float price[9], int totquan[9])
 	for(x=0;x<9 ;x++ )
 	{total_sale+=totquan[x]*price[x];
 	}
-	printf("Total sale is RM%.2f\n\n",total_sale);
+	printf("\n\t\t~~~Statistics for Current Time-space~~~\n\n");
+	printf("\n\nTotal sale is RM%.2f\n\n",total_sale);
 
-	printf("Food Code \t Quantity \t	 Sale\n");
+	printf("Food Code \t    Quantity \t	 Sale\n\n");
 	
 	for(x=0;x<9;x++)
 	printf("%s \t \t %d \t\t %.2f\n",fname[x],totquan[x],totquan[x]*price[x]);
@@ -205,6 +229,7 @@ int s,i=0,y,x,no=0,max;
 void Menu(char fname[9][50],float price[9])
 {
 	int x;
+	printf("\n\t\t~~~Cafe Ratatouille Menu~~~\n\n");
 	printf("\nFood Code \t\t Price\n");
 
 	for(x=0;x<9;x++)
@@ -213,14 +238,14 @@ void Menu(char fname[9][50],float price[9])
 }
 
 
-void Order(char fname[9][50],int quan[max][9], int totquan[9])
+void Order(char fname[9][50],int quan[max][9], int totquan[9], float price[9])
 {
 	char foodcode[5],exit;
 	int quants,y;
 
 do
 {
-	printf("Insert Order Code and quantity\n-");
+	printf("\nInsert Order Code and quantity\n-");
 	scanf("%s %d",foodcode,&quants); fflush(stdin);
 
 
@@ -246,14 +271,20 @@ do
 	quan[i][9]+=quants;
 	
 
+	printf("\nCurrent Order:\n");
 	for(y=0;y<9;y++)
 	{
 	if(quan[i][y]>0)      // FOODCODE ARRAY NEEDED //~~~~~~~~~~~~~~~~~
-	printf("\n%s\t->  %d\t\n\n",fname[y],quan[i][y]);
+	printf("\n%s ----------------- x%d\t\n\n",fname[y],quan[i][y]);
 	}
-	printf("Press [a] order or [x] to finish\n\n"); 
+	printf("\n__________________________________________________________________\n");
+	printf("Press any key to continue ordering or [x] to finish\n\n"); 
 	exit=getch();
-
+	if(exit!='x'&& exit!='X')
+	{
+	system("cls");
+    Menu(fname,price);
+	}
 }while(exit!='x'&&exit!='X');
 
 	for(y=0;y<9;y++) // FOOD CODE
@@ -279,7 +310,6 @@ void ChooseTable(int table[],int max,int a,int b)
 	check2=0;
 	printf("Choose a table\n");
 	scanf("%d",&tableno);
-	printf("\n\n~~~~~~~~%d~~~~~~~~~\n\n",table[tableno-1]);
 
 while(check2==0)	
 {	
@@ -303,14 +333,14 @@ while(check2==0)
 
 	i=tableno-1;
 	table[tableno-1]*=-1;
-
+	system("cls");
 }
 
 void Checkout(int quan[max][9], char fname[9][50],int table[max], float price[9])
 {
 	int x,y;float totp,tot=0;
 	
-	system("color B");
+	system("color F");
 	printf("Food \t \tQuantity \tprice\n");
 	table[i]*=-1;
 for(y=0;y<9;y++)
@@ -340,6 +370,9 @@ void SetupPrice(char fname[9][50],float price[9])
 	char foodcode[5],exit;
 	int x;
 	float priceNew;
+	
+	system("cls");
+	printf("\n\t\tCurrent Menu Pricing\n\n");
 	printf("\nFood Code \t\t Price\n");
 
 	for(x=0;x<9;x++)
@@ -374,6 +407,9 @@ void SetupPrice(char fname[9][50],float price[9])
 	
 	printf("\nEnter new price: RM ");
 	scanf("%f",&priceNew);
+	printf("\n\n\t\t~~Price successfully changed~~\n\n");
+	system("cls");
+	printf("\n\t\tCurrent Menu Pricing\n\n");
 	
 	if(strcmpi(foodcode,"a1")==0)
  		price[0]=priceNew;
@@ -396,16 +432,14 @@ void SetupPrice(char fname[9][50],float price[9])
 	else if(strcmpi(foodcode,"a10")==0)
 		price[9]=priceNew;
 	
-	/*for(y=0;y<9;y++)
-	{
-	if(quan[i][y]>0)      // FOODCODE ARRAY NEEDED //~~~~~~~~~~~~~~~~~
-	printf("\n%s\t->  %d\t\n\n",fname[y],quan[i][y]);
-	}*/
+	system("cls");
+	printf("\n\n\t\t~~Price successfully changed~~\n\n");
+	printf("\n\t\tCurrent Menu Pricing\n\n");
 	printf("\nFood Code \t\t Price\n");
 
 	for(x=0;x<9;x++)
 	printf("A%d\t%s \t  \t \t %.2f\n",x+1,fname[x],price[x]);
-	printf("Press [a] order or [x] to finish\n\n"); 
+	printf("\nPress any key to setup another menu item or [x] to exit\n\n"); 
 	exit=getch();
 	
 }while(exit!='x'&&exit!='X');
